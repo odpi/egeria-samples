0c1530394bf1b5d738abe03441ba26a8e9e6ba18
Mandy Chessell <mandy.e.chessell@gmail.com>
2024-10-17 12:35:53 +0100

    Create new types sample
    Signed-off-by: Mandy Chessell <mandy.e.chessell@gmail.com>

--- sample-metadata-archives/coco-metadata-archives/src/main/java/org/odpi/openmetadata/samples/archiveutilities/governanceengines/package-info.java
+++
@@ -1,8 +0,0 @@
-/* SPDX-License-Identifier: Apache 2.0 */
-/* Copyright Contributors to the ODPi Egeria project. */
-
-/**
- * Utility to build an open metadata archive containing governance engine definitions for Coco Pharmaceuticals
- */
-package org.odpi.openmetadata.samples.archiveutilities.governanceengines
-        ;
